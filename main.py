38710613f87929ebe8164a4b4b2fb10481f6330d
Monsalvo Geoffrey <monsalvo.geoffrey@gmail.com>
2024-01-22 23:34:23 +0100

    unless and until

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 import shlex
 
 code: Optional[str] = None
@@ -6,42 +6,163 @@ with open("code.asmb") as file:
     code = file.read()
 
 
-output = ""
+class Token:
+    def collapse(self):
+        pass
 
 
-for line in code.split("\n"):
+output: str = ""
+block_stack: List[Token] = []
+heap = []
+tokens: List[Token] = []
+
+
+class Raw(Token):
+    def __init__(self, code: str):
+        self.code: str = code
+
+    def collapse(self):
+        return self.code
+
+
+class Function(Token):
+    def __init__(self, name: str):
+        self.name: str = name
+        self.tokens: List[Token] = []
+
+    def append(self, token: Token):
+        self.tokens.append(token)
+
+    def collapse(self):
+        # return ""
+        res = ""
+        for token in self.tokens:
+            res += token.collapse()
+        return res
+
+
+class Until(Token):
+    def __init__(self, target):
+        self.target = target
+        self.tokens: List[Token] = []
+
+    def append(self, token: Token):
+        self.tokens.append(token)
+
+    def collapse(self):
+        code = ""
+        for token in self.tokens:
+            code += token.collapse()
+        return (
+            ("-" * self.target)
+            + "["
+            + ("+" * self.target)
+            + (code)
+            + ("-" * self.target)
+            + "]"
+            + ("+" * self.target)
+        )
+
+
+class Unless(Token):
+    def __init__(self, target):
+        self.target = target
+        self.tokens: List[Token] = []
+
+    def append(self, token: Token):
+        self.tokens.append(token)
+
+    def collapse(self):
+        code = ""
+        for token in self.tokens:
+            code += token.collapse()
+        return (
+            ("-" * self.target)
+            + "["
+            # + ("+" * self.target)
+            + (code)
+            # + ("-" * self.target)
+            + "]"
+            + ("+" * self.target)
+        )
+
+
+functions: List[Function] = []
+
+
+def parse_line(line):
+    global tokens
+    global block_stack
+    global functions
     # print(line)
     # print(shlex.split(line))
     args = shlex.split(line)
 
     if len(args) == 0:
-        continue
+        return
     cmd = args.pop(0)
     # print(args)
     # print(cmd)
 
+    block = tokens
+    if len(block_stack) > 0:
+        block = block_stack[len(block_stack) - 1]
+
     match cmd:
         case "add":
-            output += "+" * int(args[0] if len(args) > 0 else 1)
+            block.append(Raw("+" * int(args[0] if len(args) > 0 else 1)))
         case "sub":
-            output += "-" * int(args[0] if len(args) > 0 else 1)
+            block.append(Raw("-" * int(args[0] if len(args) > 0 else 1)))
         case "set":
-            output += "[-]"
-            output += "+" * int(args[0] if len(args) > 0 else 0)
+            block.append(Raw("[-]"))
+            block.append(Raw("+" * int(args[0] if len(args) > 0 else 0)))
         case "set_char":
-            output += "[-]"
-            output += "+" * ord(args[0])
+            block.append(Raw("[-]"))
+            block.append(Raw("+" * ord(args[0])))
         case "move_right":
-            output += ">" * int(args[0] if len(args) > 0 else 1)
+            block.append(Raw(">" * int(args[0] if len(args) > 0 else 1)))
         case "move_left":
-            output += "<" * int(args[0] if len(args) > 0 else 1)
+            block.append(Raw("<" * int(args[0] if len(args) > 0 else 1)))
         case "print":
-            output += "."
+            block.append(Raw("."))
         case "input":
-            output += ","
+            block.append(Raw(","))
+        case "function":
+            block_stack.append(Function(args[0]))
+        case "until":
+            block_stack.append(Until(int(args[0])))
+        case "unless":
+            block_stack.append(Unless(int(args[0])))
+        case "end":
+            current_block = block_stack.pop()
+            block = tokens
+            if len(block_stack) > 0:
+                block = block_stack[len(block_stack) - 1]
+            if isinstance(current_block, Function):
+                functions.append(current_block)
+            else:
+                block.append(current_block)
+        case "call":
+            for func in functions:
+                if func.name == args[0]:
+                    block.append(Raw(func.collapse()))
+                    break
         case _:
             pass
 
+
+def collapse():
+    global output
+    for token in tokens:
+        output += token.collapse() + "\n"
+
+
+for line in code.split("\n"):
+    parse_line(line)
+
+
+collapse()
+print(output)
 
 with open("output.bf", "w") as file:
     file.write(output)
